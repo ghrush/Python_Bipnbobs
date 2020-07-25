8d5fc319eba73eca89e9c15c78e0dbe1e465e6ee
GRush <66552567+ghrush@users.noreply.github.com>
2020-07-25 17:01:55 +0200

    Add files via upload

---
+++ custom_module.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# custom_modlule.py
+PI = 3.14159
+
+
+# In[5]:
+
+
+def plusUp2(x):
+    return x + 2
+
+
+# In[6]:
+
+
+def addUp(a, b):
+    return a + b
+
+
+# In[ ]:
